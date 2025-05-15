0c1256884f8d698474ec01581c4721c831ea49c9
Julien MABIRE <mabirej@bts-sn.lrq>
2025-05-15 14:50:45 +0200

    Mise à jour des fichiers HTML9

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Route pour la page d'accueil
+
 @app.route('/')
 def index():
-    return render_template('index.html')  # Accueil
+    return render_template('index.html')  
 
-# Route pour la page Service
+
 @app.route('/service')
 def service():
-    return render_template('service.html')  # Page Services
+    return render_template('service.html')  
 
-# Route pour la page Contact
+
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')  # Page Contact
+    return render_template('contact.html')  
 
-# Route pour la page Note de frais
+
 @app.route('/notedefrais', methods=['GET', 'POST'])
 def notedefrais():
     if request.method == 'POST':
-        # Récupération des données du formulaire
+       
         nom = request.form['nom']
         prenom = request.form['prenom']
         email = request.form['email']
@@ -33,18 +33,18 @@ def notedefrais():
         type_vehicule = request.form['type_vehicule']
         repas = request.form['repas']
         
-        # Sauvegarde des données dans un fichier CSV
+        
         with open('frais_de_deplacement.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-            # Écriture des données en ligne dans le fichier CSV
+            
             writer.writerow([nom, prenom, email, date_deplacement, lieu, distance, type_vehicule, repas])
         
-        # Redirection après la soumission du formulaire
+       
         return redirect(url_for('merci'))
 
-    return render_template('formulaire.html')  # Affichage du formulaire de frais
+    return render_template('formulaire.html')  
 
-# Route pour afficher une page de remerciement
+
 @app.route('/merci')
 def merci():
     return "Merci pour votre soumission !"
