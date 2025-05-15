7f1300b4ade2b52eef1658edce78a49f1e98207d
Julien MABIRE <mabirej@bts-sn.lrq>
2025-05-15 14:59:21 +0200

    Mise à jour des fichiers HTML98

--- app.py
+++ app.py
@@ -3,20 +3,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route pour la page d'accueil
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route pour la page des services
-@app.route('/service')
-def service():
+@app.route('/services')
+def services():
     return render_template('service.html')
 
-# Route pour la page de contact
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8808)  # Permet d'accepter les connexions externes
