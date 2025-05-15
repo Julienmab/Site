1281367791c3fa71274ece7e25268a8b30018158
Julien MABIRE <mabirej@bts-sn.lrq>
2025-05-15 14:55:03 +0200

    Mise à jour des fichiers HTML1

--- app.py
+++ app.py
@@ -1,53 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
-import csv
-from datetime import datetime
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# Route pour la page d'accueil
 @app.route('/')
-def index():
-    return render_template('index.html')  
+def home():
+    return render_template('index.html')
 
-
+# Route pour la page des services
 @app.route('/service')
 def service():
-    return render_template('service.html')  
+    return render_template('service.html')
 
-
+# Route pour la page de contact
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')  
+    return render_template('contact.html')
 
-
-@app.route('/notedefrais', methods=['GET', 'POST'])
-def notedefrais():
-    if request.method == 'POST':
-       
-        nom = request.form['nom']
-        prenom = request.form['prenom']
-        email = request.form['email']
-        date_deplacement = request.form['date_deplacement']
-        lieu = request.form['lieu']
-        distance = request.form['distance']
-        type_vehicule = request.form['type_vehicule']
-        repas = request.form['repas']
-        
-        
-        with open('frais_de_deplacement.csv', mode='a', newline='') as file:
-            writer = csv.writer(file)
-            
-            writer.writerow([nom, prenom, email, date_deplacement, lieu, distance, type_vehicule, repas])
-        
-       
-        return redirect(url_for('merci'))
-
-    return render_template('formulaire.html')  
-
-
-@app.route('/merci')
-def merci():
-    return "Merci pour votre soumission !"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8808)  # Permet d'accepter les connexions externes
